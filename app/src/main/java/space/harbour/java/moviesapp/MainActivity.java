673150e70d880379aad998b31fcd087fce29f520
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-26 20:15:08 +0700

    Working app with simple button

--- app/src/main/java/space/harbour/java/moviesapp/MainActivity.java
+++ app/src/main/java/space/harbour/java/moviesapp/MainActivity.java
@@ -3,6 +3,10 @@ package space.harbour.java.moviesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void wakeUp(View view) {
+        TextView textUp = findViewById(R.id.textView3);
+        textUp.setText("Ahmed is awake");
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(R.drawable.pulfiction);
     }
 }
